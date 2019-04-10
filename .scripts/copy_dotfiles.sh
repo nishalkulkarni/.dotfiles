ee155d936a58abb58e175439543ef24332deed19
Nishal Kulkarni <kulknishu@gmail.com>
2019-04-10 14:30:22 +0530

    to edit or not to edit

--- .scripts/copy_dotfiles.sh
+++ .scripts/copy_dotfiles.sh
@@ -5,10 +5,18 @@ echo "Started copying..."
 # Scripts
 rsync -vahP --delete ~/.scripts ~/Documents/dotfiles/
 
-# Config Files
+# Config Folder
 rsync -vahP --delete ~/.config/i3 ~/Documents/dotfiles/.config
 rsync -vahP --delete ~/.config/i3blocks ~/Documents/dotfiles/.config
 rsync -vahP --delete ~/.config/rofi ~/Documents/dotfiles/.config
 rsync -vahP --delete ~/.config/compton.conf ~/Documents/dotfiles/.config
+
+# Other files
+rsync -vahP --delete ~/.bashrc ~/Documents/dotfiles/
+rsync -vahP --delete ~/.bash_profile ~/Documents/dotfiles/
+rsync -vahP --delete ~/.profile ~/Documents/dotfiles/
+rsync -vahP --delete ~/.xinitrc ~/Documents/dotfiles/
+rsync -vahP --delete ~/.Xclients ~/Documents/dotfiles/
+
 
 echo "Finished copying."
