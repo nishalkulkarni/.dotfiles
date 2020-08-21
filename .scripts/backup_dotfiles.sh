5a63f851e247f74a48bd7c0a62c4a8ca5f04191c
Nishal Kulkarni <kulknishu@gmail.com>
2020-08-21 10:01:46 +0530

    add dunst for notifications

--- .scripts/backup_dotfiles.sh
+++ .scripts/backup_dotfiles.sh
@@ -13,6 +13,7 @@ rsync -vahP --delete ~/.config/rofi ~/Documents/dotfiles/.config
 rsync -vahP --delete ~/.config/compton.conf ~/Documents/dotfiles/.config
 rsync -vahP --delete ~/.config/xmobar ~/Documents/dotfiles/.config
 rsync -vahP --delete ~/.config/alacritty ~/Documents/dotfiles/.config
+rsync -vahP --delete ~/.config/dunst ~/Documents/dotfiles/.config
 
 # Other folders
 rsync -vahP --delete ~/.xmonad ~/Documents/dotfiles/.xmonad
