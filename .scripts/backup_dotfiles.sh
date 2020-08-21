3891095437864b578d7278aaba76d77477d6cca1
Nishal Kulkarni <kulknishu@gmail.com>
2020-08-21 08:58:53 +0530

    Add xmonad, xmobar & dmenu folders to backup

--- .scripts/backup_dotfiles.sh
+++ .scripts/backup_dotfiles.sh
@@ -8,8 +8,13 @@ rsync -vahP --delete ~/.scripts ~/Documents/dotfiles/
 # Config folder
 rsync -vahP --delete ~/.config/i3 ~/Documents/dotfiles/.config
 rsync -vahP --delete ~/.config/i3blocks ~/Documents/dotfiles/.config
+rsync -vahP --delete ~/.config/dmenu ~/Documents/dotfiles/.config
 rsync -vahP --delete ~/.config/rofi ~/Documents/dotfiles/.config
 rsync -vahP --delete ~/.config/compton.conf ~/Documents/dotfiles/.config
+rsync -vahP --delete ~/.config/xmobar ~/Documents/dotfiles/.config
+
+# Other folders
+rsync -vahP --delete ~/.xmonad ~/Documents/dotfiles/.xmonad
 
 # Other files
 rsync -vahP --delete ~/.bashrc ~/Documents/dotfiles/
